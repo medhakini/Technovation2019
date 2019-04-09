6ffb3539da4cd167264d5031bc16d694ef2fc6e6
Kalie <kaliefaith@gmail.com>
2019-04-08 22:28:23 -0700

    Adjustments to the main+home pages

--- app/src/main/java/com/example/technovation2019/HomeActivity.java
+++ app/src/main/java/com/example/technovation2019/HomeActivity.java
@@ -1,13 +1,52 @@
 package com.example.technovation2019;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import java.util.Locale;
+import android.widget.Toast;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends Activity {
+    // initialized for text to speech
+    TextToSpeech t1;
+    EditText ed1;
+    Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_main);
+        ed1 = (EditText) findViewById(R.id.editText);
+        b1 = (Button)findViewById(R.id.button);
+
+        t1 = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    t1.setLanguage(Locale.UK);
+                }
+            }
+        });
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String toSpeak = ed1.getText().toString();
+                Toast.makeText(getApplicationContext(), toSpeak,Toast.LENGTH_SHORT).show();
+                t1.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
+    }
+
+    public void onPause(){
+        if(t1 != null){
+            t1.stop();
+            t1.shutdown();
+        }
+        super.onPause();
     }
 }
+
--- app/src/main/java/com/example/technovation2019/MainActivity.java
+++ app/src/main/java/com/example/technovation2019/MainActivity.java
@@ -2,77 +2,39 @@ package com.example.technovation2019;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import java.util.Locale;
 import android.content.Intent;
 
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
-    TextToSpeech t1;
-    EditText ed1;
-    Button b1;
 
-   public static TextView resultTextView;
+
+    // initialized for qr code / barcode scanning
+    public static TextView resultTextView;
     Button scan_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ed1 = (EditText) findViewById(R.id.editText);
-        b1 = (Button) findViewById(R.id.button);
-
-        t1 = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
-            @Override
-            public void onInit(int status) {
-                if (status != TextToSpeech.ERROR) {
-                    t1.setLanguage(Locale.UK);
-                }
-            }
-        });
-
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String toSpeak = ed1.getText().toString();
-                Toast.makeText(getApplicationContext(), toSpeak, Toast.LENGTH_SHORT).show();
-                t1.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
-            }
-        });
-
         resultTextView = (TextView) findViewById(R.id.result_text);
-        scan_btn = (Button) findViewById(R.id.btn_scan);
+        // refers to button that will start the scanning
+        scan_btn = (Button) findViewById(R.id.btn_scan_me);
 
         scan_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 startActivity(new Intent(getApplicationContext(), ScanCodeActivity.class));
-
             }
         });
     }
 
-    public void onPause(){
-        if(t1 !=null){
-            t1.stop();
-            t1.shutdown();
-        }
-        super.onPause();
-    }
-
     public void buttonOnClick(View v){
         Button button = (Button) v;
-        ((Button) v).setText("clicked");
+        ((Button) v).setText("Clicked");
         startActivity(new Intent(getApplicationContext(), HomeActivity.class));
     }
-
-
 }
 
 
